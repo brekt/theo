6cc84d060f96c082fa43c4495b33b1fcf547b3ee
mtburnett <mike.burnett@blippar.com>
2017-07-13 11:39:08 -0400

    New component sub folder working and poly class working.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Poly, { shapes } from './Poly';
 
 /* exported variableName */
 
 export default class App extends Component {
+	constructor() {
+		super();
+		const cube = new Poly(shapes.CUBE);
+		console.log(cube.getShapeName());
+	}
 	render() {
 		return (
 			<div>
--- src/components/Poly.js
+++ src/components/Poly.js
@@ -1,6 +1,40 @@
-export default class Poly {
-	constructor(faceCount = 6) {
-		
-	}
+export const shapes = {
+	TETRAHEDRON: {
+		faceCount: 4,
+		shapeName: 'tetrahedron',
+	},
+	PENTAHEDRON: {
+		faceCount: 5,
+		shapeName: 'pentahedron',
+	},
+	CUBE: {
+		faceCount: 6 ,
+		shapeName: 'cube',
+	},
+	SEPTAHEDRON: {
+		faceCount: 7,
+		shapeName: 'septahedron',
+	},
+	OCTAHEDRON: {
+		faceCount: 8,
+		shapeName: 'octahedron',
+	},
+	NONAHEDRON: {
+		faceCount: 9,
+		shapeName: 'nonahedron',
+	},
+	DECAHEDRON: {
+		faceCount: 10,
+		shapeName: 'decahedron',
+	},
+};
 
+export default class Poly {
+	constructor(type = shapes.CUBE) {
+		this.faceCount = type.faceCount;
+		this.shapeName = type.shapeName;
+	}
+	getShapeName() {
+		return this.shapeName;
+	}
 }
