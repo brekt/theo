f03071e0e905fab24bffe5eaf8883f4c7d390681
mtburnett <mike.burnett@blippar.com>
2017-07-21 18:23:41 -0400

    Rethinking using React. Have working poly class in poly_class.html file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import Poly, { shapes } from './Poly';
+import Scene from './Scene.js';
 
 export default class App extends Component {
-	constructor() {
-		super();
-		const cube = new Poly(shapes.CUBE);
-		console.log(cube.getShapeName());
-		console.log(THREE.WebGLRenderer);
-	}
 	render() {
 		return (
-			<div>
-				Hello world.
+			<div id="container">
+				<Scene />
 			</div>
 		);
 	}
